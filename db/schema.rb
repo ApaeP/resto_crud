7581a644b9b128cf817c348066d164948e90251e
Paul Portier <paulportier@gmail.com>
2022-11-15 10:41:28 +0100

    CRUD restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_081026) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_092512) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.text "description"
   end
 
 end
